57001ce618e63bd6169a8644508b275557301b48
sam <sgrowe@live.co.uk>
2016-04-15 14:36:44 +0100

    Use production mode on heroku

--- Volla/settings.py
+++ Volla/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p-zelz*2j$k-n6(b#i^1n4pnx53^zjw@)nln^+yr4o%h84#g#)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('VOLLA_DEV') == 'TRUE'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.volla.co']
 
 
 # Application definition
---
+++ stories/fields.py
@@ -0,0 +1,21 @@
+from django.db import models
+from hashids import Hashids
+
+default_hashids = Hashids()
+
+
+class HashidField(models.AutoField):
+    def __init__(self, *args, **kwargs):
+        self.hashids = kwargs.pop('hashid', default=default_hashids)
+        super().__init__(*args, **kwargs)
+
+    def from_db_value(self, value, expression, connection, context):
+        int_id = super().from_db_value(value, expression, connection, context)
+        return self.hashids.encode([int_id])
+
+    def get_prep_value(self, value):
+        decoded = self.hashids.decode(value)
+        if len(decoded) != 1:
+            raise ValueError('The hashid {!r} did not decode to a single int, but instead to {!r}'.format(value, decoded))
+        int_id = decoded[0]
+        return super().get_prep_value(int_id)
